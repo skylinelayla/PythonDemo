68275eb88d095dab81215994d32b1e17f14ecd56
skykun <skylinelayla2012@gmail.com>
2015-12-18 16:57:02 +0800

    嵌套列表

--- Demo/HelloWorld.py
+++ Demo/HelloWorld.py
@@ -51,8 +51,48 @@ print(S.upper())
 print('%s,eggs,and %s'%('spam','SPAM!'))
 print('{0},eggs,and {1}'.format('spam','SPAM!'))#高级格式化
 
+L=[123,'spam',1.23]
+L.append('NI')
+L.pop(2)
+print(L)#大多数列表的方法可以直接改变列表的值
+M=['bb','aa','cc']
+M.sort()
+print(M)
+M.reverse()#翻转
+print(M)
+M=[[1,2,3],[4,5,6],[1,8,9]]
+print(M)
+print(M[1])
+#列表解析假设我们需要从矩阵中提取第二列
+col2=[row[1] for row in M]
+print(col2)
+#print(row[1]+1 for row in M)
+diag=[M[i][i] for i in [0,1,2]]
+print(diag)
 
+douebles=[c*2 for c in 'spam']
+print(douebles)
+G=(sum(row) for row in M)
+print(next(G))
+#字典
+D={'food':'spam','quantity':4,'color':'pink'}
+print(D['food'])
+print(D['quantity']+1)
+D['quantity']+=1
+D['name']='Bob'
+D['job']='dev'
 
+print(D)
+
+res={'name':{'first':'Bob','last':'Smith'},'job':['dev','mgr'],'age':45.5}
+
+print(res)
+print(res['name'])
+print(res['name']['first'])
+print(res['name']['last'])
+print(res['job'][0])
+print(res['job'].append('janitor'))
+print(res)
 
 
 
