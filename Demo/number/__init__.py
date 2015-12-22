d345d44f9961bd609f98ad88fee3bdea74d5f975
skykun <skylinelayla2012@gmail.com>
2015-12-22 20:44:53 +0800

    set

---
+++ Demo/number/__init__.py
@@ -0,0 +1,15 @@
+#repr 用以显示额外的细节，str用以一般用途
+num=1/3
+print(repr(num))
+print(str(num))
+#Floor 除法,取整，截断除法
+X=2
+Y=4
+print(X//Y)
+
+#floor
+import math
+math.floor(2.5)
+math.floor(-2.5)
+math.trunc(2.5)
+math.trunc(-2.5)
---
+++ Demo/number/collection.py
@@ -0,0 +1,13 @@
+#集合对象
+x=set('abcde')
+y=set('bdxyz')
+print(x)
+print(y)
+print('e' in x)
+print(x-y)
+print(x|y)
+print(x&y)
+print(x^y)#剔除相同的然后联合
+
+print(x>y,x<y)
+
